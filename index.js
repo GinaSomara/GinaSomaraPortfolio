1167ce361135239a80b6e9bfd1326d1ec9118009
Gina Somara <ginasomara15@gmail.com>
2021-08-19 01:18:45 -0400

    Port 3000 working
    The index page displays using express and npm start. However the css, nor the images are loading.

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
+// === VARIABLES === //
 var express = require('express');
-var expressHBS = require('express-handlebars');
 var app = express();
-var router = express.Router();
-app.engine('handlebars', expressHBS());
+var handlebars = require("express-handlebars");
+var path = require("path");
+var router = express.Router();  //creates a router object
+
+ //===== view ENGINE SET UP =====//
 app.set('view engine', 'handlebars');
+app.engine(
+    "hbs",
+    handlebars({
+      layoutsDir: path.join(__dirname, "views/layouts"),
+      partialsDir: path.join(__dirname, "views/partials"), 
+      extname: ".hbs", //expected file extension for handlebars files
+      defaultLayout: "layout" //default layout for app, general template for all pages in app
+    })
+);
+app.set("views", path.join(__dirname, "views")); 
+app.set("view engine", "hbs");
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(express.static('images'));
 
 
 //===== .GET PAGES =====//
-router.get('/', (res, next) => { 
-    res.render('index', {title: 'Home Page', css:['style.css'], js:[]});
+router.get('/', (req, res, next) => { 
+    res.render('index', {title: 'Home Page', css:['style.css']});
 });
+app.use('/', router);
 
-router.get('/project1', (res, next) => {
+router.get('/project1', (req, res, next) => {
     res.render('project1', {title: 'Data Structures', css:['projects.css'], js:[]});
 });
 
-router.get('/project2', (res, next) => {
+router.get('/project2', (req, res, next) => {
     res.render('project2', {title: 'Fountain Website', css:['projects.css'], js:[]});
 });
 
-router.get('/project3', (res, next) => {
+router.get('/project3', (req, res, next) => {
     res.render('project3', {title: 'This Portfolio', css:['projects.css'], js:[]});
 });
 
