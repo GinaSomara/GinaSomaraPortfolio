ba44103a20d45229c2ad9c9e024ed882e2c3e2b0
Gina Somara <ginasomara15@gmail.com>
2021-08-27 11:00:36 -0400

    Changing files names

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.get('/project2', (req, res, next) => {
     res.render('../public/views/fountainWebsite/html/home', {title: 'Gina Somara Project', css:['../public/css/fountainWebsite/home.css']});
 });
 
-app.get('/project3', (req, res, next) => {
-    res.render('../public/views/partials/project3', {title: 'Gina Somara Project', css:['../public/css/projects.css'], js:['../public/js/navBar.js']});
+app.get('/this-portfolio', (req, res, next) => {
+    res.render('../public/views/partials/this-portfolio', {title: 'Gina Somara Portfolio', css:['../public/css/projects.css'], js:['../public/js/navBar.js']});
 });
 
 app.get('/project4', (req, res, next) => {
